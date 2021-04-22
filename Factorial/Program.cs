ef728237bfa97c1e0b5e7108f246148599d31334
sohamdhamnaskar <sohamdham123@gmail.com>
2021-04-22 10:53:18 +0530

    Added: Factorial in cs

---
+++ Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, fact = 1;
+            Console.WriteLine("Enter a number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++) 
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial of " + num + " is : " + fact);
+            Console.ReadLine();
+        }
+    }
+}
